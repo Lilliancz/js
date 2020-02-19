a104f65ddf2743f1cdf1626ec9e79a86575a8c7c
Lilliancz <12296128+Lilliancz@users.noreply.github.com>
2020-02-19 14:06:35 -0500

    add additional saved variables and autotimer

--- time_on_task.js
+++ time_on_task.js
@@ -2,6 +2,7 @@
 // a huge thanks to Šime Vidas who wrote the base code on
 // https://stackoverflow.com/questions/5766263/run-settimeout-only-when-tab-is-active
 // to set the embedded data, you MUST preset the variables in the survey flow prior to the block
+// variables to set: socialSec, focalSec, autoLimit
 
 Qualtrics.SurveyEngine.addOnload(function()
 {
@@ -24,6 +25,10 @@ var elapsedSec;
 var socialSec;	
 var focalSec;
 var popup = 0;
+var limit = "${q://QID6/SelectedChoicesRecode}";
+var autoLimit = "${e://Field/autoLimit}"
+// the autoLimit is the number of seconds allowed for the entire question including focal and nonfocal tasks.
+// it is set in the survey flow
 	
 var logup = document.querySelector('#logup');
 
@@ -41,7 +46,7 @@ var logup = document.querySelector('#logup');
 	//if the focal task window is not focused, run this code
 	if ( document.hidden ) { 		  
 		  //if the social task duration exceeds the number, then pop up a new tab
-		  if (socialSec > 10&&popup == 0){
+		  if (socialSec > limit && popup == 0){
 		  	var url = "https://umich.qualtrics.com/jfe/form/SV_9nMLFWoqyYBHhvD";
 		  	window.open(url, '_blank'); 
 		  	window.focus();
@@ -58,24 +63,25 @@ var logup = document.querySelector('#logup');
 	//calculate elapsed time
 	nowTime = new Date().getTime() / 1000;	
 	elapsedSec = nowTime - startTime; 
-	
+	  
+	  // if the responded has reached the limit for the entire question, stop the interval, 
+	  // record the data, and advance to the next question.
+	  if ( elapsedSec > autoLimit ){
+		  clearInterval(tid);
+		 Qualtrics.SurveyEngine.setEmbeddedData( 'socialSec', socialSec );
+		Qualtrics.SurveyEngine.setEmbeddedData( 'focalSec', focalSec );	
+		  $('NextButton').click();
+	  }
 	//change the elapsed time text on screen
 	timeText.textContent = "elapsed time is " + elapsedSec.toFixed(1) + ", time spent on other tasks is " + socialSec;
   }, delta);
 })();
 	
-	// on next button click, set the embedded data for focal and nonfocal tasks
-	jQuery('#NextButton').on('click', function() {
-	//alert("social sec is "+socialSec);
-		Qualtrics.SurveyEngine.setEmbeddedData( 'socialSec', socialSec );
-		Qualtrics.SurveyEngine.setEmbeddedData( 'focalSec', focalSec );	
-	})
-
 	
 });
 
 Qualtrics.SurveyEngine.addOnUnload(function()
 {
 	/*Place your JavaScript here to run when the page is unloaded*/
-	clearInterval(tid);
+
 });
