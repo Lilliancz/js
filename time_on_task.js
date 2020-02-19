9d08776371727d3851600b96d12514946452a2b2
Lilliancz <12296128+Lilliancz@users.noreply.github.com>
2020-02-19 11:52:02 -0500

    update time on task

--- time_on_task.js
+++ time_on_task.js
@@ -1,7 +1,13 @@
-//this code presumes you have a DIV called "logup" and a DIV called "textTime" to show the timer text. 
+// this code presumes you have a DIV called "logup" and a DIV called "textTime" to show the timer text. 
 // a huge thanks to Šime Vidas who wrote the base code on
 // https://stackoverflow.com/questions/5766263/run-settimeout-only-when-tab-is-active
+// to set the embedded data, you MUST preset the variables in the survey flow prior to the block
 
+Qualtrics.SurveyEngine.addOnload(function()
+{
+	/*Place your JavaScript here to run when the page loads*/
+
+});
 Qualtrics.SurveyEngine.addOnload(function()
 {
 	/*Place your JavaScript here to run when the page loads*/
@@ -16,22 +22,24 @@ var timeText = document.querySelector('#timeText');
 var nowTime;	
 var elapsedSec;	
 var socialSec;	
+var focalSec;
 var popup = 0;
 	
 var logup = document.querySelector('#logup');
 
 (function() {
-  var time = 0,
-      delta = 200,
-      tid;
+	//rate of data acquisition in ms is the delta
+  	var time = 0,	
+      		delta = 100,
+      		tid;
 
   tid = setInterval(function() {
 	  
-	  //get the time spent on the social tasks
-	  socialSec = elapsedSec - time/1000; 
-
-	  //if the focal task window is not focused, run this code
-	  if ( document.hidden ) { 		  
+	//get the time spent on the social tasks
+	socialSec = elapsedSec - time/1000; 
+		
+	//if the focal task window is not focused, run this code
+	if ( document.hidden ) { 		  
 		  //if the social task duration exceeds the number, then pop up a new tab
 		  if (socialSec > 10&&popup == 0){
 		  	var url = "https://umich.qualtrics.com/jfe/form/SV_9nMLFWoqyYBHhvD";
@@ -40,26 +48,34 @@ var logup = document.querySelector('#logup');
 		  	popup = 1;
 	  	}    
 	}
-    
+    	//round to integer
+	socialSec = socialSec.toFixed(1);
 	//incremented time for each interval
 	time += delta;
-    logup.textContent = (time / 1000).toFixed(1) + ' seconds spent on the focal task' ;
-
-	  //calculate elapsed time
-	  nowTime = new Date().getTime() / 1000;	
+    	
+	logup.textContent = (time / 1000).toFixed(1) + ' seconds spent on the focal task' ;
+	focalSec = (time / 1000).toFixed(1);
+	//calculate elapsed time
+	nowTime = new Date().getTime() / 1000;	
 	elapsedSec = nowTime - startTime; 
 	
-	  //change the elapsed time text on screen
-	timeText.textContent = "elapsed time is " + elapsedSec.toFixed(1) + ", time spent on other tasks is " + socialSec.toFixed(1);
+	//change the elapsed time text on screen
+	timeText.textContent = "elapsed time is " + elapsedSec.toFixed(1) + ", time spent on other tasks is " + socialSec;
   }, delta);
-	
 })();
 	
+	// on next button click, set the embedded data for focal and nonfocal tasks
+	jQuery('#NextButton').on('click', function() {
+	//alert("social sec is "+socialSec);
+		Qualtrics.SurveyEngine.setEmbeddedData( 'socialSec', socialSec );
+		Qualtrics.SurveyEngine.setEmbeddedData( 'focalSec', focalSec );	
+	})
+
 	
 });
 
 Qualtrics.SurveyEngine.addOnUnload(function()
 {
 	/*Place your JavaScript here to run when the page is unloaded*/
-clearInterval(tid);
+	clearInterval(tid);
 });
